d3dce5710bb5964d6bf7a37f3b6de659b565f013
Pedro Filho <pedro@filho.me>
2020-08-03 11:14:03 -0300

    make it type unsensitive and improving functions

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@ import countries from './countries.json';
 
 import { Country } from 'types';
 
-const getCountryFromName = (name: string): Country => {
-  return countries.filter(c => c.name === name)[0];
+const getCountryFromName = (name: string): Country | undefined => {
+  return countries.find(c => c.name.toUpperCase() === name.toUpperCase());
 };
 
-const getCountryFromAlpha2 = (alpha2: string): Country => {
-  return countries.filter(c => c.alpha2 === alpha2)[0];
+const getCountryFromAlpha2 = (alpha2: string): Country | undefined => {
+  return countries.find(c => c.alpha2.toUpperCase() === alpha2.toUpperCase());
 };
 
-const getCountryFromAlpha3 = (alpha3: string): Country => {
-  return countries.filter(c => c.alpha3 === alpha3)[0];
+const getCountryFromAlpha3 = (alpha3: string): Country | undefined => {
+  return countries.find(c => c.alpha3.toUpperCase() === alpha3.toUpperCase());
 };
 
-const getCountryFromCode = (code: string): Country => {
-  return countries.filter(c => c.code === code)[0];
+const getCountryFromCode = (code: string): Country | undefined => {
+  return countries.find(c => c.code === code);
 };
 
 export {
